2411f8de177ca04c8a6451172e3bab62989a11c7
Asher Fraz <asherfraz@gmail.com>
2025-09-04 14:27:14 +0500

    CHECKING: Rate Limiter

--- backend-ashstore/src/app.ts
+++ backend-ashstore/src/app.ts
@@ -7,7 +7,7 @@ import connectToDatabase from './dbConnect';
 import errorHandler from './middlewares/errorHandler';
 import userRoutes from './routes/user.routes';
 import helmet from 'helmet';
-// import rateLimit from 'express-rate-limit';
+import rateLimit from 'express-rate-limit';
 import { getCurrentReqLocation } from './middlewares/getCurrentReqLocation';
 
 
@@ -32,28 +32,27 @@ app.use(cors({
     credentials: true,
 }));
 
-// const limiter = rateLimit({
-//     // retry after 10 minutes
-//     windowMs: 10 * 60 * 1000, // 10 minutes
-//     limit: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes).
-//     standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
-//     legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
-//     ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
-//     // store: ... , // Redis, Memcached, etc. See below.
-//     message: {
-//         status: 429,
-//         message: "Too many requests from this IP, please try again later.",
-//         success: false,
-//     }
-// })
+const limiter = rateLimit({
+    // retry after 10 minutes
+    windowMs: 10 * 60 * 1000, // 10 minutes
+    limit: 100, // Limit each IP to 100 requests per `window` (here, per 10 minutes).
+    standardHeaders: 'draft-8', // draft-6: `RateLimit-*` headers; draft-7 & draft-8: combined `RateLimit` header
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers.
+    ipv6Subnet: 56, // Set to 60 or 64 to be less aggressive, or 52 or 48 to be more aggressive
+    // store: ... , // Redis, Memcached, etc. See below.
+    message: {
+        status: 429,
+        message: "Too many requests from this IP, please try again later.",
+        success: false,
+    }
+})
 
 // // Apply the rate limiting middleware to all requests.
 // app.use(limiter)
-// app.use('/api/v1/user', limiter, userRoutes);
 
 
 // REST API routes
-app.use('/api/v1/user', userRoutes);
+app.use('/api/v1/user', limiter, userRoutes);
 // app.use('/api/v1/admin', adminRoutes);
 // app.use('/api/v1/product', productRoutes);
 // app.use('/api/v1/order', orderRoutes);
