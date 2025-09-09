42ce2b3a88716d1f747c29de58793d9fc986247b
Asher Fraz <asherfraz@gmail.com>
2025-09-09 19:04:14 +0500

    FIX: Cookies Error Fixed-5

--- backend-ashstore/src/app.ts
+++ backend-ashstore/src/app.ts
@@ -23,8 +23,6 @@ app.use(
         crossOriginOpenerPolicy: false,
     })
 );
-// Enable this if you're behind a reverse proxy like Nginx or on Heroku/Vercel
-app.set('trust proxy', true);
 app.use(morgan('dev'));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true }));
